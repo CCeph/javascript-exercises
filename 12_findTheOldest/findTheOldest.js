92cdf8c3165e52e9f90abcf092742cdc0b0e86d2
cceph <yousefelsisyy@gmail.com>
2022-09-08 18:47:12 -0300

    Wanted to save this so I can work on it at home

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(mainArray) {
+    let peopleArray = [...mainArray]
+    let oldest = peopleArray.reduce((oldestPerson, person) => {
+        if (!("yearOfDeath" in person)) {
+            person.age = (new Date().getFullYear()) - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (person.age > (oldestPerson.age)) {
+            oldestPerson = person
+        }
+    
+    return oldestPerson
+    }, oldestPerson = {
+        age: 0
+    })
+    return oldest;
 };
 
 // Do not edit below this line
